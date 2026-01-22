b1b53f30313517fd651841fa4e1b41914732c931
matheus <matheuslimapereira990@gmail.com>
2026-01-22 14:44:38 -0300

    Aula01Operadores - Aritiméticos

---
+++ src/introducao/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero1 = 20;
+        int numero2 = 10;
+        double resultado = numero2/ (double)numero1;
+        System.out.println(resultado);
+        //System.out.println(numero1 - numero2);
+        //System.out.println(numero1 + numero2);
+        //System.out.println(numero1 * numero2);
+    }
+}
