931d8cbc20d31d0abbdc7910518d027c2a86843d
matheus <matheuslimapereira990@gmail.com>
2026-01-22 14:06:24 -0300

    Aula02TiposPrimitivos

---
+++ src/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main() {
+    // Tipos primitivos: byte, short, int, long, float, double, char, boolean
+    byte idadeByte = 10;
+    short idadeShort = 10;
+    int idade = 10;
+    long numeroLongo= 10000000;
+    float salarioFloat = 2500;
+    double salarioDouble = 3000;
+    char caractere = 'M';
+    boolean verdadeiro = true;
+    boolean falso = false;
+
+    System.out.println("Minha idade é: "+ idade + "Anos.");
+
+    }
+}
