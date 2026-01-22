c468125977bf13d195d464c8a6884b2c8cc5a189
matheus <matheuslimapereira990@gmail.com>
2026-01-22 14:30:08 -0300

    Aula05TiposPrimitivos- exercicios

---
+++ src/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Matheus";
+        String endereco = "Rua sebastião";
+        double salario = 2600.50;
+        String data = "27/10/2023";
+        String relatorio = "Eu "+nome+" morando no endereço "+endereco+" confirmo que recebi o salario "+salario+" na data "+ data;
+        System.out.println(relatorio);
+        //System.out.println("Eu "+nome+" morando no endereço "+endereco+" confirmo que recebi o salario "+salario+" na data "+ data);
+    }
+}
