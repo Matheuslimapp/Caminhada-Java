37eaf10521fa4cd5d3de3c81e9873f16c57ee6e8
matheus <matheuslimapereira990@gmail.com>
2026-01-22 14:01:11 -0300

    Aula01

---
+++ src/Aula01.java
@@ -0,0 +1,10 @@
+public class Aula01 {
+    static void main() {
+        // isso e um comentario de uma linha
+
+        /* Comentarios de mutiplas linhas
+        */
+
+        System.out.println("Ola, Mundo!");
+    }
+}
