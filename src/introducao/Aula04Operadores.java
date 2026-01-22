d7cf71750ce2a50ca9fbbbc9fdeb10ba9986b1a0
matheus <matheuslimapereira990@gmail.com>
2026-01-22 15:29:23 -0300

    Aula02Operadores - Lógicos AND

--- src/introducao/Aula04Operadores.java
+++ src/introducao/Aula04Operadores.java
@@ -25,5 +25,15 @@ public class Aula04Operadores {
         System.out.println("isDezMenorQueVinte "+ isDezMenorQueVinte);
         System.out.println("isDezIgualQueVinte "+ isDezIgualQueVinte);
         System.out.println("isDezIgualQueDez "+ isDezIgualQueDez);
+
+
+        // &&(and) ||(or) !(not)
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario > 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+ isDentroDaLeiMaiorQueTrinta );
+        System.out.println("isDentroDaLeiMenorQueTrinta "+ isDentroDaLeiMenorQueTrinta );
     }
 }
