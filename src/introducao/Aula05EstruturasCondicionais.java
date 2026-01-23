b5552f6f0a6b17f4e88e15281c0298897e5ad2cf
matheus <matheuslimapereira990@gmail.com>
2026-01-23 14:21:50 -0300

    Aula01Estruturas Condicionais - IF

---
+++ src/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        //!
+        if (isAutorizadoComprarBebida){
+            System.out.println("Autorizado a comprar bebida alcólica");
+
+        if (!isAutorizadoComprarBebida){
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+
+
+
+        }
+        }
+    }
+}
