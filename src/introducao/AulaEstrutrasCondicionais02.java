73666692f3881b879675a4076daaa83dd3fd5a5c
matheus <matheuslimapereira990@gmail.com>
2026-01-23 14:35:18 -0300

    Aula03Estruturas Condicionais - ELSE IF

---
+++ src/introducao/AulaEstrutrasCondicionais02.java
@@ -0,0 +1,21 @@
+package introducao;
+
+public class AulaEstrutrasCondicionais02 {
+    public static void main(String[] args) {
+
+
+        // idade < 15 - infantil
+        // idade >= 15 && idade < 18 - Junvenil
+        // idade >= 18 - adulto
+        int idade = 17;
+        String categoria;
+        if (idade < 15){
+            categoria = "Infantil";
+        }else if (idade >= 15 && idade < 18) {
+            categoria = "Juvenil";
+        }else{
+            categoria = "Adulto";
+        }
+        System.out.println(categoria);
+    }
+}
