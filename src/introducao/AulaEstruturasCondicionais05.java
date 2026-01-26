8c88448296cf6ea9c6ffef45b5fb9cc7f016df8b
matheus <matheuslimapereira990@gmail.com>
2026-01-26 01:03:09 -0300

    Aula05Estruturas Condicionais - Switch

---
+++ src/introducao/AulaEstruturasCondicionais05.java
@@ -0,0 +1,48 @@
+package introducao;
+
+public class AulaEstruturasCondicionais05 {
+    public static void main(String[] args) {
+
+        byte dia = 10;
+
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Opção invalida");
+                break;
+        }
+        char sexo = 'M';
+        switch (sexo){
+            case 'M':
+                System.out.println("Homen");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Invalido");
+                break;
+        }
+
+    }
+}
