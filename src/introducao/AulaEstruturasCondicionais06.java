78dbcd6b24bfcba1a5c162836f2a0fe3b454f3bb
matheus <matheuslimapereira990@gmail.com>
2026-01-26 21:26:40 -0300

    Aula06Estruturas Condicionais - exercicio Switch

---
+++ src/introducao/AulaEstruturasCondicionais06.java
@@ -0,0 +1,24 @@
+package introducao;
+
+public class AulaEstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte dia = 4;
+        switch (dia){
+            case 1:
+            case 7:
+                System.out.println("Final de Semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia Util");
+                break;
+            default:
+                System.out.println("Opção invalida");
+                break;
+
+        }
+    }
+}
