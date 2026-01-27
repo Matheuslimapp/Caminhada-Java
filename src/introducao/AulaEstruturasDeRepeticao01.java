1cb2dcf766646a20db6775cf008582e55b2e8313
matheus <matheuslimapereira990@gmail.com>
2026-01-26 21:34:13 -0300

    Aula01Estruturas De Repetição - Laços de repetição

---
+++ src/introducao/AulaEstruturasDeRepeticao01.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class AulaEstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // While, Do While, For
+        int count = 0;
+        while (count < 10){
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while " + ++count);
+        }while (count < 10);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("For "+ i);
+        }
+    }
+}
