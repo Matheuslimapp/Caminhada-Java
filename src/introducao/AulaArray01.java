852c2b782c1499ada3872189c76872d2324db68f
matheus <matheuslimapereira990@gmail.com>
2026-01-29 20:58:10 -0300

    Aula01Arrays

---
+++ src/introducao/AulaArray01.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class AulaArray01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 20;
+        idades[1] = 67;
+        idades[2] = 450;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
