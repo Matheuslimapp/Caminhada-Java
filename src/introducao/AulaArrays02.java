5f920d80cc2e7aeb315df112c5a4450e335cc72b
matheus <matheuslimapereira990@gmail.com>
2026-01-29 21:04:42 -0300

    Aula02Arrays

---
+++ src/introducao/AulaArrays02.java
@@ -0,0 +1,14 @@
+package introducao;
+
+public class AulaArrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float e double = 0
+        // char '\u0000' = ' '
+        //boolean = false
+        //String null
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
