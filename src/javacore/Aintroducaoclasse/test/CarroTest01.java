ba2e08fe12ca2d5ab4bd0f8cd78b806f7fb730d2
matheus <matheuslimapereira990@gmail.com>
2026-02-07 15:50:03 -0300

    Orientação Objetos - Exercício Classes

---
+++ src/javacore/Aintroducaoclasse/test/CarroTest01.java
@@ -0,0 +1,23 @@
+package javacore.Aintroducaoclasse.test;
+
+import javacore.Aintroducaoclasse.domino.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro carro1 = new Carro();
+
+        carro.nome = "GT-R";
+        carro.modelo = "Skyline";
+        carro.ano = 2007;
+
+        carro1.nome = "Fusca";
+        carro1.modelo = "Volkswagen";
+        carro1.ano = 1959;
+
+        System.out.println("=========Menu De Carros=========");
+        System.out.println("Nome: "+carro.nome+"\nModelo: "+carro.modelo+"\nAno: "+carro.ano+"\n");
+        System.out.println("Nome: "+carro1.nome+"\nModelo: "+carro1.modelo+"\nAno: "+carro1.ano+"\n");
+
+    }
+}
