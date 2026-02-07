75787af0ed6af962d500e79297b58684496279a1
matheus <matheuslimapereira990@gmail.com>
2026-02-07 16:16:19 -0300

    Orientação Objetos - Métodos01

---
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10 + 13);
+    }
+    public void subtraiDoisNumeros(){
+        System.out.println(21 - 2);
+    }
+}
