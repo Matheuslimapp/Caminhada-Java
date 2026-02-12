3e7483fe3933e4d0aa043febc80ec19403781a18
matheus <matheuslimapereira990@gmail.com>
2026-02-12 14:27:53 -0300

    Orientação Objetos - Modificador de acesso01 - private, get e set

---
+++ src/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,14 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(60);
+        //pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
