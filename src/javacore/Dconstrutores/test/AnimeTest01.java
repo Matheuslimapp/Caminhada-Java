f285ced41f8442063d33a666524d96f8cb93868e
matheus <matheuslimapereira990@gmail.com>
2026-02-13 21:48:25 -0300

    Orientação Objetos - Construtores 02 - Sobrecarga

--- src/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,8 +5,7 @@ import javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu","TV", 12,"Ação");
-        Anime anime1 = new Anime();
+        Anime anime = new Anime("Haikyuu","TV", 12,"Ação","TG");
         anime.imprime();
     }
 }
