88cdf250cb4d3082349e0e9768d874460cc3b0d3
matheus <matheuslimapereira990@gmail.com>
2026-02-16 15:44:58 -0300

    Orientação Objetos - Associação01 - Arrays com Objetos

---
+++ src/javacore/GAssociacao/test/JogadorTest01.java
@@ -0,0 +1,16 @@
+package javacore.GAssociacao.test;
+
+import javacore.GAssociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pele");
+        Jogador jogador2 = new Jogador("Neymar");
+        Jogador jogador3 = new Jogador("cr7");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+
+    }
+}
