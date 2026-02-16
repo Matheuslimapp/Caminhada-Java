3458bd9f14ec4229bc6f55caba4cb9e6ca3c62f7
matheus <matheuslimapereira990@gmail.com>
2026-02-15 23:42:28 -0300

    Orientação Objetos - Métodos estáticos

--- src/javacore/Fmodificadorstatio/test/CarroTest01.java
+++ src/javacore/Fmodificadorstatio/test/CarroTest01.java
@@ -4,12 +4,11 @@ import javacore.Fmodificadorstatio.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 200);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
-        System.out.println(Carro.velocidadeLimite);
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite);
 
         c1.imprime();
         c2.imprime();
