bd93a57b28e3d222d02f0f59995125ed226f0135
matheus <matheuslimapereira990@gmail.com>
2026-02-17 17:22:56 -0300

    Orientação Objetos - Herança01

---
+++ src/javacore/Heranca/test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package javacore.Heranca.test;
+
+import javacore.Heranca.dominio.Endereco;
+import javacore.Heranca.dominio.Funcionario;
+import javacore.Heranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Deku");
+        pessoa.setCpf("111111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Gojou");
+        funcionario.setCpf("22222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(1200);
+
+        System.out.println("------------");
+        funcionario.imprime();
+    }
+}
