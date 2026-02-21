6f6982fb1cd23282a3c082f8f017056525bec67f
matheus <matheuslimapereira990@gmail.com>
2026-02-20 21:59:53 -0300

    Orientação Objetos - Polimorfismo02 - Funcionamento

---
+++ src/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,20 @@
+package javacore.Npolimorfismo.test;
+
+import javacore.Npolimorfismo.dominio.Computador;
+import javacore.Npolimorfismo.dominio.Produto;
+import javacore.Npolimorfismo.dominio.Tomate;
+import javacore.Npolimorfismo.service.CalculadoraImposto;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("-----------");
+        Produto produto1 = new Tomate("Americano",20);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.calcularImposto());
+    }
+}
