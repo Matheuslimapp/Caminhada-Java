d7ac827dd5e4a00279009b78e44d9a0834745df6
matheus <matheuslimapereira990@gmail.com>
2026-02-21 18:40:39 -0300

    Orientação Objetos - Polimorfismo03 - Parâmetros

--- src/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package javacore.Npolimorfismo.test;
 
 import javacore.Npolimorfismo.dominio.Computador;
+import javacore.Npolimorfismo.dominio.Televisao;
 import javacore.Npolimorfismo.dominio.Tomate;
 import javacore.Npolimorfismo.service.CalculadoraImposto;
 
@@ -8,9 +9,11 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
        Computador computador = new Computador("NUC10i7", 11000);
        Tomate tomate = new Tomate("Tomatão", 10);
-       CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao televisao = new Televisao("Samsung 50\" ", 50);
+       CalculadoraImposto.calcularImposto(computador);
         System.out.println("-----------------------");
-       CalculadoraImposto.calcularImpostoTomate(tomate);
-
+       CalculadoraImposto.calcularImposto(tomate);
+       System.out.println("-----------------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
