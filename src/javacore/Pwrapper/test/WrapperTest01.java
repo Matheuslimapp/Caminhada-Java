3b319cf5cf9b6f77c33261a74641694a9525b899
matheus <matheuslimapereira990@gmail.com>
2026-03-01 19:45:32 -0300

    Classes Utilitárias - Wrappers01

---
+++ src/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,26 @@
+package javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        //Primitivos
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'M';
+        boolean booleanP = false;
+
+        //Wrappers
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'M';
+        Boolean booleanW = false;
+
+    }
+}
