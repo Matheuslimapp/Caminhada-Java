1a55194423080c94f9d20854abd4435993e26059
matheus <matheuslimapereira990@gmail.com>
2026-03-07 14:49:54 -0300

    Classes Utilitárias - IO02 - FileWriter

---
+++ src/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,17 @@
+package javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true);){
+            fw.write("Eu amo JAVA\nPula linhaaa");
+            fw.flush();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
