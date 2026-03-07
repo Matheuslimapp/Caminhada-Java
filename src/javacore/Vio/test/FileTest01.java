a083370a460a8f0574d53155419e79d2f8e0af14
matheus <matheuslimapereira990@gmail.com>
2026-03-07 14:41:30 -0300

    Classes Utilitárias - IO01 - File

---
+++ src/javacore/Vio/test/FileTest01.java
@@ -0,0 +1,29 @@
+package javacore.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class FileTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try {
+            //boolean delete = file.delete();
+            boolean isCreated = file.createNewFile();
+            System.out.println("Created "+isCreated);
+            System.out.println("path "+ file.getPath());
+            System.out.println("path absolute "+ file.getAbsolutePath());
+            System.out.println("is directory  "+ file.isDirectory());
+            System.out.println("is file  "+ file.isFile());
+            System.out.println("is hidden  "+ file.isHidden());//ve se o arquivo e oculto
+            System.out.println("Last modified " + Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()));
+            boolean exists = file.exists();//Ve se o arquivo existi
+            if (exists){
+                System.out.println("Deleted "+ file.delete());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
