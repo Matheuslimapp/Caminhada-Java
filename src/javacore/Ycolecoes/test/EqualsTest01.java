b538d63132dcd90ee67876bf5e25b24068d59ae2
matheus <matheuslimapereira990@gmail.com>
2026-03-11 19:11:04 -0300

    Coleções02 - equals 02

--- src/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/javacore/Ycolecoes/test/EqualsTest01.java
@@ -4,8 +4,8 @@ import javacore.Ycolecoes.dominio.Smartphone;
 
 public class EqualsTest01 {
     public static void main(String[] args) {
-        Smartphone s1 = new Smartphone("1ABC1", "Iphine");
-        Smartphone s2 = new Smartphone("1ABC1", "Iphine");
+        Smartphone s1 = new Smartphone("1ABC1", "Iphone");
+        Smartphone s2 = new Smartphone("1ABC1", "Iphone");
         System.out.println(s1.equals(s2));
 
     }
