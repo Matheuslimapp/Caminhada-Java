0c2cb478f114a96a30c0088c65c70a501240fe43
matheus <matheuslimapereira990@gmail.com>
2026-03-18 14:51:37 -0300

    Coleções07 - List pt 02

--- src/javacore/Ycolecoes/test/ListTest01.java
+++ src/javacore/Ycolecoes/test/ListTest01.java
@@ -6,16 +6,29 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList(16); //1.4
+        List<String> nomes2 = new ArrayList(16);
         nomes.add("Matheus");
-        nomes.add("DevDojo AScademy");
+        nomes.add("DevDojo");
+        nomes2.add("suane");
+        nomes2.add("Academy");
+
+        //System.out.println(nomes.remove("Matheus"));
+        nomes.addAll(nomes2);
 
         for (String nome: nomes){
             System.out.println(nome);
         }
-        nomes.add("Suane");
+
         System.out.println("-------------");
-        for (int i = 0; i < nomes.size(); i++){
+        int size = nomes.size();
+        for (int i = 0; i < size; i++){
+            nomes.add("Suane");
             System.out.println(nomes.get(i));
         }
+        System.out.println(nomes);
+
+        List<Integer> numeros = new ArrayList<>();//Lista tem que ser objeto
+        numeros.add(1);
+
     }
 }
