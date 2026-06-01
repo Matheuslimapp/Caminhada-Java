2e5629b3736b993b152f67904fd705ccbb0b00d1
Matheuslimapp <matheuslimapereira990@gmail.com>
2026-06-01 20:27:12 -0300

    Lambdas01 - predicate

--- src/javacore/ZZBcomportamentos/interfaces/CarPredicate.java
+++ src/javacore/ZZBcomportamentos/interfaces/CarPredicate.java
@@ -1,7 +1,10 @@
 package javacore.ZZBcomportamentos.interfaces;
 
 import javacore.ZZBcomportamentos.dominio.Car;
-
+@FunctionalInterface
 public interface CarPredicate {
+    // anonimas, funções, conciso(Reduzir Codigo)
     boolean test(Car car);
+    //(parametro) -> exepressão
+    //(Car car) -> car.getColor().equals("green");
 }
