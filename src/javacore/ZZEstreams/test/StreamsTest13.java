eb8d09779f2f3cb9d1b3b59c395c0b37e534c5b8
matheuslimapp <matheuslimapereira990@gmail.com>
2026-09-23 16:04:54 -0300

    Streams13 - Collectors pt 03 - Grouping by pt 02

---
+++ src/javacore/ZZEstreams/test/StreamsTest13.java
@@ -0,0 +1,33 @@
+package javacore.ZZEstreams.test;
+
+import javacore.ZZEstreams.dominio.Category;
+import javacore.ZZEstreams.dominio.LightNovel;
+import javacore.ZZEstreams.dominio.Promotion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamsTest13 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlod", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99,Category.FANTASY),
+            new LightNovel("Fullnelal Alcheisl", 5.99,Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Mongatari", 4.00, Category.ROMANCE)
+    ));
+
+    public static void main(String[] args) {
+        Map<Promotion, List<LightNovel>> collect = lightNovels.stream()
+                .collect(Collectors.groupingBy(ln -> ln.getPrice() < 6 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE));
+        System.out.println(collect);
+        Map<Category, Map<Promotion, List<LightNovel>>> collect1 = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory,
+                Collectors.groupingBy(ln -> ln.getPrice() < 6 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE)));
+        System.out.println(collect1);
+    }
+}
