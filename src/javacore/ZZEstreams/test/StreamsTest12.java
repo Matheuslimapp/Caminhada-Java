7dd5c542795978c069575e87fb322689fb45a0d6
matheuslimapp <matheuslimapereira990@gmail.com>
2026-09-23 15:28:55 -0300

    Streams12 - Collectors pt 02 - Grouping by pt 01

---
+++ src/javacore/ZZEstreams/test/StreamsTest12.java
@@ -0,0 +1,46 @@
+package javacore.ZZEstreams.test;
+
+import javacore.ZZEstreams.dominio.Category;
+import javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamsTest12 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlod", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99,Category.FANTASY),
+            new LightNovel("Fullnelal Alcheisl", 5.99,Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Mongatari", 4.00, Category.ROMANCE)
+    ));
+
+    public static void main(String[] args) {
+        Map<Category, List<LightNovel>> categoryLightNovelMap = new HashMap<>();
+        List<LightNovel> fantasy = new ArrayList<>();
+        List<LightNovel> drama = new ArrayList<>();
+        List<LightNovel> romance = new ArrayList<>();
+        for (LightNovel lightNovel : lightNovels) {
+            switch (lightNovel.getCategory()){
+                case FANTASY: fantasy.add(lightNovel); break;
+                case ROMANCE: romance.add(lightNovel); break;
+                case DRAMA: drama.add(lightNovel); break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + lightNovel.getCategory());
+            }
+        }
+        categoryLightNovelMap.put(Category.DRAMA, drama);
+        categoryLightNovelMap.put(Category.FANTASY, fantasy);
+        categoryLightNovelMap.put(Category.ROMANCE, romance);
+        System.out.println(categoryLightNovelMap);
+
+        Map<Category, List<LightNovel>> collect = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory));
+        System.out.println(collect);
+    }
+}
